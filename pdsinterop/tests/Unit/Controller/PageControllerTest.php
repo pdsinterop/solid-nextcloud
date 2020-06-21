05e7e17231555b727d919fa6ca29ae6ec4713ac5
Auke van Slooten <auke@muze.nl>
2020-06-21 16:21:19 +0200

    skeleton of the solid nextcloud interop app

---
+++ pdsinterop/tests/Unit/Controller/PageControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OCA\PDSInterop\Tests\Unit\Controller;
+
+use PHPUnit_Framework_TestCase;
+
+use OCP\AppFramework\Http\TemplateResponse;
+
+use OCA\PDSInterop\Controller\PageController;
+
+
+class PageControllerTest extends PHPUnit_Framework_TestCase {
+	private $controller;
+	private $userId = 'john';
+
+	public function setUp() {
+		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+
+		$this->controller = new PageController(
+			'pdsinterop', $request, $this->userId
+		);
+	}
+
+	public function testIndex() {
+		$result = $this->controller->index();
+
+		$this->assertEquals('index', $result->getTemplateName());
+		$this->assertTrue($result instanceof TemplateResponse);
+	}
+
+}
