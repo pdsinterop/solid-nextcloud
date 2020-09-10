e011d900d1c42c7016bba1e7c4c139aa25428b76
Michiel de Jong <michiel@unhosted.org>
2020-09-10 16:15:46 +0200

    openid-configuration in PageController

--- solid/lib/Controller/PageController.php
+++ solid/lib/Controller/PageController.php
@@ -46,4 +46,13 @@ class PageController extends Controller {
 	public function turtleProfile() {
 		return new TemplateResponse('solid', 'turtle-profile', [], 'blank');
 	}
+
+	/**
+	 * @PublicPage
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 */
+	public function openid() {
+		return(' { "issuer":"https://solid.community", "jwks_uri":"https://solid.community/jwks", "response_types_supported":[ "code", "code token", "code id_token", "id_token code", "id_token", "id_token token", "code id_token token", "none" ], "token_types_supported":[ "legacyPop", "dpop" ], "response_modes_supported":[ "query", "fragment" ], "grant_types_supported":[ "authorization_code", "implicit", "refresh_token", "client_credentials" ], "subject_types_supported":["public"], "id_token_signing_alg_values_supported":["RS256"], "token_endpoint_auth_methods_supported":"client_secret_basic", "token_endpoint_auth_signing_alg_values_supported":["RS256"], "display_values_supported":[], "claim_types_supported":["normal"], "claims_supported":[], "claims_parameter_supported":false, "request_parameter_supported":true, "request_uri_parameter_supported":false, "require_request_uri_registration":false, "check_session_iframe":"https://solid.community/session", "end_session_endpoint":"https://solid.community/logout", "authorization_endpoint":"https://solid.community/authorize", "token_endpoint":"https://solid.community/token", "userinfo_endpoint":"https://solid.community/userinfo", "registration_endpoint":"https://solid.community/register" } ');
+	}
 }
