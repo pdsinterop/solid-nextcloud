b2057b893fb7832a28ec50f67de3e72779a8a79c
Yvo Brevoort <yvo@muze.nl>
2020-09-16 12:36:33 +0200

    adding dummy endpoints

--- solid/appinfo/routes.php
+++ solid/appinfo/routes.php
@@ -10,12 +10,19 @@
 return [
     'routes' => [
         ['name' => 'page#openid', 'url' => '/openid', 'verb' => 'GET'],
-        ['name' => 'page#authorize', 'url' => '/authorize', 'verb' => 'GET'],
         ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
         ['name' => 'page#do_echo', 'url' => '/echo', 'verb' => 'POST'],
         ['name' => 'page#profile', 'url' => '/@{userId}/', 'verb' => 'GET'],
         ['name' => 'page#turtleProfile', 'url' => '/@{userId}/turtle', 'verb' => 'GET' ],
         ['name' => 'page#dataJson', 'url' => '/@{userId}/data.json', 'verb' => 'GET' ],
-        ['name' => 'page#cors', 'url' => '/{path}', 'verb' => 'OPTIONS', 'requirements' => array('path' => '.+') ]
+        ['name' => 'page#cors', 'url' => '/{path}', 'verb' => 'OPTIONS', 'requirements' => array('path' => '.+') ],
+
+        ['name' => 'page#authorize', 'url' => '/authorize', 'verb' => 'GET'],
+        ['name' => 'page#jwks', 'url' => '/jwks', 'verb' => 'GET'],
+        ['name' => 'page#session', 'url' => '/session', 'verb' => 'GET'],
+        ['name' => 'page#logout', 'url' => '/logout', 'verb' => 'GET'],
+        ['name' => 'page#token', 'url' => '/token', 'verb' => 'GET'],
+        ['name' => 'page#userinfo', 'url' => '/userinfo', 'verb' => 'GET'],
+        ['name' => 'page#register', 'url' => '/register', 'verb' => 'POST']
     ]
 ];
