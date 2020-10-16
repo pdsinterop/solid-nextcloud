db1efd2963b22d8a06d3aae703fefa24b56ffd90
Michiel de Jong <michiel@unhosted.org>
2020-10-16 11:17:14 +0200

    Temporarily comment out tear-down steps

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
+
 # Run the Solid test-suite
 docker network create testnet
 docker build -t base https://github.com/pdsinterop/test-suites.git#master:/servers/nextcloud-server/base
 docker build -t nextcloud-server https://github.com/pdsinterop/test-suites.git#master:/servers/nextcloud-server/server
 docker run -d --name server --network=testnet nextcloud-server
 
-docker build -t webid-provider https://github.com/pdsinterop/test-suites.git#master:/testers/webid-provider
+docker build -t webid-provider hhttps://github.com/pdsinterop/test-suites.git#master:/testers/webid-provider
 docker build -t cookie         https://github.com/pdsinterop/test-suites.git#master:servers/nextcloud-server/cookie
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/nextcloud-server/env.list
 until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
@@ -25,7 +26,7 @@ docker exec -u root -it server service apache2 reload
 echo Getting cookie...
 export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file /tmp/env-vars-for-test-image.list cookie`"
 docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list webid-provider
-rm /tmp/env-vars-for-test-image.list
-docker stop server
-docker rm server
-docker network remove testnet
+# rm /tmp/env-vars-for-test-image.list
+# docker stop server
+# docker rm server
+# docker network remove testnet
