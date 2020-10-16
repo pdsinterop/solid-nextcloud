9e112c5fff6866d8e757c95c4fcfd126b3ea2778
Michiel de Jong <michiel@unhosted.org>
2020-10-16 10:45:06 +0200

    Use images from PDS Interop test suite

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -2,9 +2,11 @@
 
 # Run the Solid test-suite
 docker network create testnet
-docker build -t nextcloud-server https://github.com/solid/test-suite.git#master:/servers/nextcloud-server
-docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
+docker build -t nextcloud-server https://github.com/pdsinterop/test-suites.git#master:/servers/nextcloud-server
 docker run -d --name server --network=testnet nextcloud-server
+
+docker build -t webid-provider hhttps://github.com/pdsinterop/test-suites.git#master:/testers/webid-provider
+docker build -t cookie         https://github.com/pdsinterop/test-suites.git#master:servers/nextcloud-server/cookie
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/nextcloud-server/env.list
 until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
 do
@@ -15,7 +17,10 @@ do
 done
 docker ps -a
 docker logs server
-docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list webid-provider
+
+echo Getting cookie...
+export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file /tmp/env-vars-for-test-image.list cookie`"
+docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list webid-provider
 rm /tmp/env-vars-for-test-image.list
 docker stop server
 docker rm server
