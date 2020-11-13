a43f63b1a971b3052847aff6b14080b890f8e941
Michiel de Jong <michiel@unhosted.org>
2020-11-13 13:15:09 +0100

    Prepare for other tests

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -27,13 +27,17 @@ function startSolidNextcloud {
   docker exec -u www-data -it -e SERVER_ROOT=https://$1 $1 sh /init.sh
   docker exec -u root -it $1 service apache2 reload
   echo Getting cookie for $1...
-  export COOKIE_$1="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file ./env-vars-$1.list cookie`"
+  export COOKIE_$1="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file ./env-vars-$1.list michielbdejong/nextcloud-cookie`"
 }
 
 function runTests {
   docker pull solidtestsuite/$1-tests
   echo "Running $1 tests against server with cookie $COOKIE_server"
-  docker run --rm --network=testnet --env COOKIE="$COOKIE_server" --env-file ./env-vars-server.list solidtestsuite/$1-tests
+  docker run --rm --network=testnet \
+    --env COOKIE="$COOKIE_server" \
+    --env COOKIE_ALICE="$COOKIE_server" \
+    --env COOKIE_BOB="$COOKIE_thirdparty" \
+    --env-file ./env-vars-testers.list solidtestsuite/$1-tests
 }
 
 # ...
