cf9e00c41cf3dea0963c8b441049e9cccf4c64a7
Michiel de Jong <michiel@unhosted.org>
2020-11-13 11:59:47 +0100

    docker pull solidtestsuite/webid-provider-tests

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -8,7 +8,8 @@ docker network create testnet
 docker build -t server .
 docker run -d --name server --network=testnet server
 
-docker build -t webid-provider https://github.com/pdsinterop/test-suites.git#master:/testers/webid-provider
+docker pull solidtestsuite/webid-provider-tests
+docker tag solidtestsuite/webid-provider-tests webid-provider
 docker build -t cookie         https://github.com/pdsinterop/test-suites.git#master:servers/nextcloud-server/cookie
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/nextcloud-server/env.list
 until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
