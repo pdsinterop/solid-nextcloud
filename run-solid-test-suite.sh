3a73646892a5df32848461e4358be3e056fed851
Michiel de Jong <michiel@unhosted.org>
2020-11-13 12:41:23 +0100

    docker pull michielbdejong/nextcloud-cookie

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -9,10 +9,9 @@ docker build -t server .
 docker run -d --name server --network=testnet server
 
 docker pull solidtestsuite/webid-provider-tests
-docker tag solidtestsuite/webid-provider-tests webid-provider
-docker build -t cookie         https://github.com/pdsinterop/test-suites.git#master:servers/nextcloud-server/cookie
-wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/nextcloud-server/env.list
-until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
+docker pull michielbdejong/nextcloud-cookie
+
+until docker run --rm --network=testnet solidtestsuite/webid-provider-tests curl -kI https://server 2> /dev/null > /dev/null
 do
   echo Waiting for server to start, this can take up to a minute ...
   docker ps -a
@@ -26,9 +25,9 @@ docker exec -u www-data -it -e SERVER_ROOT=https://server server sh /init.sh
 docker exec -u root -it server service apache2 reload
 
 echo Getting cookie...
-export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file /tmp/env-vars-for-test-image.list cookie`"
-echo "Running webid-provider tests with cookie $COOKIE"
-docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list webid-provider
+export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file ./env-vars-for-test-image.list michielbdejong/nextcloud-cookie`"
+echo "Running solidtestsuite/webid-provider-tests with cookie $COOKIE"
+docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file ./env-vars-for-test-image.list solidtestsuite/webid-provider-tests
 # rm /tmp/env-vars-for-test-image.list
 # docker stop server
 # docker rm server
