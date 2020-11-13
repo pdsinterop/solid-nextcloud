4ff3c9fbe89606d1483c99f99359c4258263610b
Michiel de Jong <michiel@unhosted.org>
2020-11-13 12:49:42 +0100

    Test runner with functions

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 set -e
 
-# Run the Solid test-suite
-docker network create testnet
+function setup {
+  docker network create testnet
+  docker build -t solid-nextcloud .
+  docker pull michielbdejong/nextcloud-cookie
+}
+function teardown {
+  docker stop `docker ps --filter network=testnet -q`
+  docker rm `docker ps --filter network=testnet -qa`
+  docker network remove testnet
+}
 
-# Build and start Nextcloud server with code from current repo contents:
-docker build -t server .
-docker run -d --name server --network=testnet server
+function startSolidNextcloud {
+  docker run -d --name $1 --network=testnet solid-nextcloud
+  until docker run --rm --network=testnet solidtestsuite/webid-provider-tests curl -kI https://$1 2> /dev/null > /dev/null
+  do
+    echo Waiting for $1 to start, this can take up to a minute ...
+    docker ps -a
+    docker logs $1
+    sleep 1
+  done
 
-docker pull solidtestsuite/webid-provider-tests
-docker pull michielbdejong/nextcloud-cookie
+  docker logs $1
+  echo Running init script for Nextcloud $1 ...
+  docker exec -u www-data -it -e SERVER_ROOT=https://$1 $1 sh /init.sh
+  docker exec -u root -it $1 service apache2 reload
+  echo Getting cookie for $1...
+  export COOKIE_$1="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file ./env-vars-$1.list cookie`"
+}
 
-until docker run --rm --network=testnet solidtestsuite/webid-provider-tests curl -kI https://server 2> /dev/null > /dev/null
-do
-  echo Waiting for server to start, this can take up to a minute ...
-  docker ps -a
-  docker logs server || true
-  sleep 1
-done
-docker ps -a
-docker logs server
-echo Running init script for Nextcloud server ...
-docker exec -u www-data -it -e SERVER_ROOT=https://server server sh /init.sh
-docker exec -u root -it server service apache2 reload
+function runTests {
+  docker pull solidtestsuite/$1-tests
+  echo "Running $1 tests against server with cookie $COOKIE_server"
+  docker run --rm --network=testnet --env COOKIE="$COOKIE_server" --env-file ./env-vars-server.list solidtestsuite/$1-tests
+}
 
-echo Getting cookie...
-export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file ./env-vars-for-test-image.list michielbdejong/nextcloud-cookie`"
-echo "Running solidtestsuite/webid-provider-tests with cookie $COOKIE"
-docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file ./env-vars-for-test-image.list solidtestsuite/webid-provider-tests
-# rm /tmp/env-vars-for-test-image.list
-# docker stop server
-# docker rm server
-# docker network remove testnet
+# ...
+teardown
+setup
+startSolidNextcloud server
+runTests webid-provider
+teardown
