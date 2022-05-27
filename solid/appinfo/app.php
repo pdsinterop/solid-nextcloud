e4a50dd092fa00e158d5b848af8ff9b953e6a6c5
Yvo Brevoort <yvo@muze.nl>
2022-05-27 16:09:02 +0200

    updating AppInfo

--- solid/appinfo/app.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	declare(strict_types=1);
-
-	$app = \OC::$server->query(\OCA\Solid\AppInfo\SolidApp::class);
---
+++ solid/lib/AppInfo/Application.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Solid\AppInfo;
+
+use OC\AppFramework\Utility\TimeFactory;
+use OC\Authentication\Events\AppPasswordCreatedEvent;
+use OC\Authentication\Token\IProvider;
+use OC\Server;
+
+use OCA\Solid\Service\UserService;
+use OCA\Solid\WellKnown\OpenIdConfigurationHandler;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\IAppContainer;
+use OCP\Defaults;
+use OCP\IServerContainer;
+use OCP\Settings\IManager;
+use OCP\Util;
+
+class Application extends App implements IBootstrap {
+	public const APP_ID = 'solid';
+
+	/**
+	 * @param array $urlParams
+	 */
+	public function __construct(array $urlParams = []) {
+		parent::__construct(self::APP_ID, $urlParams);
+	}
+
+	public function register(IRegistrationContext $context): void {
+		$context->registerWellKnownHandler(SecurityTxtHandler::class);
+		$context->registerWellKnownHandler(\OCA\Solid\WellKnown\OpenIdConfigurationHandler::class);
+
+		/**
+		 * Core class wrappers
+		 */
+
+		$context->registerService('UserService', function($c) {
+			return new \OCA\Solid\Service\UserService(
+				$c->query('UserSession')
+			);
+		});
+		$context->registerService('UserSession', function($c) {
+			return $c->query('ServerContainer')->getUserSession();
+		});
+
+		// currently logged in user, userId can be gotten by calling the
+		// getUID() method on it
+		$context->registerService('User', function($c) {
+			return $c->query('UserSession')->getUser();
+		});
+
+	}
+
+	public function boot(IBootContext $context): void {}
+}
