5a2147241baa375e9e712d889a58a01cc742f32b
Yvo Brevoort <yvo@muze.nl>
2022-05-27 14:56:13 +0200

    use full namespace

--- solid/lib/AppInfo/SolidApp.php
+++ solid/lib/AppInfo/SolidApp.php
@@ -34,8 +34,7 @@ class SolidApp extends App implements IBootstrap {
     }
 
     public function register(IRegistrationContext $context): void {
-        include_once __DIR__ . '/../../vendor/autoload.php';
-        $context->registerWellKnownHandler(OpenIdConfigurationHandler::class);
+        $context->registerWellKnownHandler(\OCA\Solid\WellKnown\OpenIdConfigurationHandler::class);
     }
 
     public function boot(IBootContext $context): void {}
