eceb172a18bd4b84707ee4caf8e4a1212789218d
Yvo Brevoort <yvo@muze.nl>
2022-05-28 08:36:22 +0200

    check for solidNavigation (sharing screen does not set it)

--- solid/templates/navigation/index.php
+++ solid/templates/navigation/index.php
@@ -1,4 +1,6 @@
+<?php if (isset($_['solidNavigation'])) { ?>
 <ul>
     <li><a href="<?php p($_['solidNavigation']['launcher']); ?>"><img src="/core/img/actions/star-dark.svg"><span>Solid Apps</span></a></li>
     <li><a data-simply-command="profile" href="<?php p($_['solidNavigation']['profile']);?>"><img src="/core/img/actions/user.svg"><span>Profile</span></a></li>
 </ul>
+<?php } ?>
