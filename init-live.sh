d45c8ab352b4c5836db1f040d13017411e9b2642
Michiel de Jong <michiel@pondersource.com>
2022-06-17 13:56:58 +0000

    ref https://github.com/pdsinterop/solid-nextcloud/issues/76#issuecomment-1158896202

--- init-live.sh
+++ init-live.sh
@@ -3,7 +3,7 @@ export PHP_MEMORY_LIMIT="512M"
 php console.php maintenance:install --admin-user admin --admin-pass $MARIADB_ROOT_PASSWORD --database "mysql" --database-name "nextcloud" --database-user "root" --database-pass "$MARIADB_ROOT_PASSWORD" --database-host "127.0.0.1"
 php console.php status
 php console.php app:enable solid
-sed -i '96 i\  RewriteRule ^\\.well-known/openid-configuration /apps/solid/openid [R=302,L]' /var/www/html/.htaccess
+sed -i '96 i\  RewriteRule ^\\.well-known/openid-configuration /apps/solid/openid [PT,L]' /var/www/html/.htaccess
 sed -i "25 i\    1 => 'server'," /var/www/html/config/config.php
 sed -i "26 i\    2 => 'nextcloud.local'," /var/www/html/config/config.php
 sed -i "27 i\    3 => 'thirdparty'," /var/www/html/config/config.php
