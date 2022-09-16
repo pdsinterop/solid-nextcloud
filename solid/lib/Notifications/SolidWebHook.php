05f24c8591b43fb09cbb1046a9e3069a3d185694
Yvo Brevoort <yvo@muze.nl>
2022-09-16 15:27:22 +0200

    Setting up notification channels.

---
+++ solid/lib/Notifications/SolidWebHook.php
@@ -0,0 +1,56 @@
+<?php
+    namespace OCA\Solid\Notifications;
+
+    class SolidWebhook implements SolidNotificationsInterface
+    {
+        public function send($path, $type) {
+            $subscribedUrls = $this->getSubscribedUrls($path, $type);
+            foreach ($subscribedUrls as $url) {
+                $this->postUpdate($url, $path, $type);
+            }
+        }
+        private function getSubscribedUrls($path, $type) {
+        }
+        private function postUpdate($url, $path, $type) {
+            try {
+                $postData = $this->createUpdatePayload($path, $type);
+                $opts = array('http' =>
+                    array(
+                        'method'  => 'POST',
+                        'header'  => 'Content-Type: application/ld+json',
+                        'content' => $postData
+                    )
+                );
+                $context  = stream_context_create($opts);
+                $result = file_get_contents($webhook, false, $context);
+            } catch (\Exception $exception) {
+                throw new Exception('Could not write to webhook server', 502, $exception);
+            }
+        }
+        private function createUpdatePayload($path, $type) {
+            //$updateId = "urn:uuid:<uuid>";
+            //$actor = "<WebID>";
+            $object = [
+                "id" => $path,
+                "type" => [
+                    "http://www.w3.org/ns/ldp#Resource"
+                ]
+            ];
+            $state = "1234-5678-90ab-cdef-12345678";
+            $published = date(DATE_ISO8601);
+            $payload = [
+                "@context" => [
+                    "https://www.w3.org/ns/activitystreams",
+                    "https://www.w3.org/ns/solid/notification/v1"
+                 ],
+            //    "id":$updateId,
+            //    "actor" : [$actor],
+                "type" => [$type],
+                "object" => $object,
+            //    "state" : $state
+                "published" => $published
+            ];
+            
+            return json_encode($payload);
+        }
+    }
