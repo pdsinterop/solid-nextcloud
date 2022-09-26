938e0c48780c54b9235fc4a12f9bd3d26259c938
Michiel de Jong <michiel@unhosted.org>
2022-09-26 17:04:35 +0200

    webid-provider-tests v2.1.0

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -6,8 +6,8 @@ function setup {
   docker build -t solid-nextcloud .
   docker build -t pubsub-server  https://github.com/pdsinterop/php-solid-pubsub-server.git#main
   docker pull michielbdejong/nextcloud-cookie
-  docker pull solidtestsuite/webid-provider-tests:v2.0.3
-  docker tag solidtestsuite/webid-provider-tests:v2.0.3 webid-provider-tests
+  docker pull solidtestsuite/webid-provider-tests:v2.1.0
+  docker tag solidtestsuite/webid-provider-tests:v2.1.0 webid-provider-tests
   docker pull solidtestsuite/solid-crud-tests:v6.0.0
   docker tag solidtestsuite/solid-crud-tests:v6.0.0 solid-crud-tests
   docker pull solidtestsuite/web-access-control-tests:v7.1.0
