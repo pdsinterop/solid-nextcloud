aaf8b334990dffe4cf3d42307e5164762ec23093
Michiel de Jong <michiel@unhosted.org>
2022-10-03 16:56:58 +0200

    Try with v7.0.2

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -8,8 +8,8 @@ function setup {
   docker pull michielbdejong/nextcloud-cookie
   docker pull solidtestsuite/webid-provider-tests:v2.1.0
   docker tag solidtestsuite/webid-provider-tests:v2.1.0 webid-provider-tests
-  docker pull solidtestsuite/solid-crud-tests:v7.0.0
-  docker tag solidtestsuite/solid-crud-tests:v7.0.0 solid-crud-tests
+  docker pull solidtestsuite/solid-crud-tests:v7.0.2
+  docker tag solidtestsuite/solid-crud-tests:v7.0.2 solid-crud-tests
   docker pull solidtestsuite/web-access-control-tests:v7.1.0
   docker tag solidtestsuite/web-access-control-tests:v7.1.0 web-access-control-tests
 }
