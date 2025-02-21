7b4e1acf92fcfb85267b4da3d614a1ef58b608a7
Ben Peachey <ben@muze.nl>
2025-02-21 19:45:44 +0100

    Add usage documentation to the Nextcloud Publish script.

--- bin/publish-to-nextcloud-store.sh
+++ bin/publish-to-nextcloud-store.sh
@@ -2,6 +2,37 @@
 
 set -o errexit -o errtrace -o nounset -o pipefail
 
+# ==============================================================================
+#                           PUBLISH TO NEXTCLOUD STORE
+# ------------------------------------------------------------------------------
+# This script takes all steps needed to publish a release of solid-nextcloud to
+# the Nextcloud App store. This consists of:
+#
+# 1. Building a package
+#    - checking out the created tag
+#    - installing the project dependencies
+#    - creating a tarball from the project and its dependencies
+# 2. Deploying the package'
+#    - getting the URL from GitHub to where the tarball should be uploaded
+#    - uploading the tarball to GitHub
+# 3. Creating a release in the Nextcloud App store'
+#    - creating a signature file for the tarball
+#    - publishing the tarball to the Nextcloud store
+#
+# There are various assumption made by this script:
+#
+# - A git tag has been created
+# - A GitHub Release has been created (so a Release Page exists where a package can be uploaded to)
+# - The `transfer/solid.key` and `transfer/solid.crt` files exist (both are needed to create a signature)
+#
+# ------------------------------------------------------------------------------
+#
+# Usage example:
+#
+#     bash bin/publish_to_nextcloud_store.sh 'v0.9.0'
+#
+# ==============================================================================
+
 publish_to_nextcloud_store() {
 
 }
