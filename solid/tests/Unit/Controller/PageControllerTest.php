0a5733f6010cc208cdb44ca3e414407107d7c4ee
Ben Peachey <potherca@gmail.com>
2025-06-01 20:25:04 +0200

    Fix broken unit-test.

--- solid/tests/Unit/Controller/PageControllerTest.php
+++ solid/tests/Unit/Controller/PageControllerTest.php
@@ -1,25 +1,47 @@
 <?php
 
-namespace OCA\Solid\Tests\Unit\Controller;
-
-use PHPUnit_Framework_TestCase;
+namespace OCA\Solid\Controller;
 
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IConfig;
+use OCP\IRequest;
+use OCP\IURLGenerator;
+use OCP\IUserManager;
+use PHPUnit\Framework\TestCase;
 
-use OCA\Solid\Controller\PageController;
-
-
-class PageControllerTest extends PHPUnit_Framework_TestCase {
+/**
+ * @coversDefaultClass \OCA\Solid\Controller\PageController
+ * @covers ::__construct
+ */
+class PageControllerTest extends TestCase
+{
+	private const MOCK_USER_ID = 'mock-user';
 	private $controller;
-	private $userId = 'john';
 
-	public function setUp() {
-		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+	public function setUp(): void
+	{
+		$mockConfig = $this->getMockBuilder(IConfig::class)->getMock();
+		$mockRequest = $this->getMockBuilder(IRequest::class)->getMock();
+		$mockUrlGenerator = $this->getMockBuilder(IURLGenerator::class)->getMock();
+		$mockUserManager = $this->getMockBuilder(IUserManager::class)->getMock();
 
-		$this->controller = new PageController('solid', $request, $this->userId);
+		$this->controller = new PageController(
+			'solid',
+			$mockRequest,
+			$mockConfig,
+			$mockUserManager,
+			$mockUrlGenerator,
+			self::MOCK_USER_ID
+		);
 	}
 
-	public function testIndex() {
+	/**
+	 * @covers ::index
+	 * @uses \OCA\Solid\BaseServerConfig::__construct
+	 * @uses \OCA\Solid\ServerConfig::__construct
+	 */
+	public function testIndex()
+	{
 		$result = $this->controller->index();
 
 		$this->assertEquals('index', $result->getTemplateName());
