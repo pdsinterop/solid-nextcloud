5daf9f7583dc91058f8f4efcd348fb9685a65416
Ben Peachey <github@pother.ca>
2025-06-06 16:30:49 +0200

    Use Secure WebSocket (#180)
    Resolution for CLN-001.
    
    * Use Secure WebSocket
    * update pubsub image
    * update websocket client code
    * update to https

--- solid/lib/Notifications/SolidPubSub.php
+++ solid/lib/Notifications/SolidPubSub.php
@@ -12,18 +12,19 @@
         }
 
         public function send($path, $type) {
-            $pubsub = str_replace(["https://", "http://"], "ws://", $this->pubsub);
+            $pubsub = str_replace(["https://", "http://"], "wss://", $this->pubsub);
 
-            $client = new Client($pubsub, array(
-                'headers' => array(
-                    'Sec-WebSocket-Protocol' => 'solid-0.1'
-                )
-            ));
-
+            $client = new Client($pubsub);
+            $client->setContext(['ssl' => [
+                'verify_peer'       => false, // if false, accept SSL handshake without client certificate
+                'verify_peer_name'  => false,
+                'allow_self_signed' => true,
+            ]]);
+            $client->addHeader("Sec-WebSocket-Protocol", "solid-0.1");
             try {
-                $client->send("pub $path\n");
+                $client->text("pub $path\n");
             } catch (\WebSocket\Exception $exception) {
-                throw new Exception('Could not write to pubsub server', 502, $exception);
+                throw new \Exception('Could not write to pubsub server', 502, $exception);
             }
         }
     }
