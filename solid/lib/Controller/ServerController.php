80ee0f920b1464a0058b94a215568a8c9445abb8
Yvo Brevoort <yvo@muze.nl>
2025-06-26 20:11:15 +0200

    straight from GET or POST

--- solid/lib/Controller/ServerController.php
+++ solid/lib/Controller/ServerController.php
@@ -104,8 +104,12 @@ class ServerController extends Controller
 	}
 
 	private function createAuthServerConfig() {
-		$request = \Laminas\Diactoros\ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
-		$clientId = $request->getParsedBody()['client_id'];
+                $clientId = null;
+                if (isset($_GET['client_id'])) {
+                        $clientId = $_GET['client_id'];
+                } else if (isset($_POST['client_id'])) {
+                        $clientId = $_POST['client_id'];
+                }
 		$client = $this->getClient($clientId);
 		$keys = $this->getKeys();
 		try {
