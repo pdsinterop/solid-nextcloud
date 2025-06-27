a9fdac7655ee3e8411e1d6e3558bf2546ca608a4
Yvo Brevoort <yvo@muze.nl>
2025-06-27 14:40:05 +0200

    implement all functions to get something to run

--- solid/lib/ClientAuth.php
+++ solid/lib/ClientAuth.php
@@ -8,13 +8,41 @@
 	 * @package OCA\Solid
 	 */
 	class ClientAuth extends ABackend implements ICheckPasswordBackend {
-	        private $config;
-
-		public function __construct(IConfig $config) {
-		    $this->config = $config;
+		public function __construct() {
+			error_log("SO Constructed solid client auth backend");
 		}
 
-                public function checkPassword(string $username, string $password) {
-                    return true;
-                }
-	}
+		public function checkPassword(string $username, string $password) {
+			error_log("SO checking password for $username");
+			return true;
+		}
+
+		public function getBackendName() {
+			error_log("SO getBackendName");
+			return "Solid";
+		}
+		public function deleteUser($uid) {
+			error_log("SO deleteUser");
+			return false;
+		}
+		public function getUsers($search = "", $limit = null, $offset = null, $callback = null) {
+			error_log("SO getUsers");
+			return [];
+		}
+		public function userExists($uid) {
+			error_log("SO User exists");
+			return true;
+		}
+		public function getDisplayName($uid) {
+			error_log("SO getDisplayName");
+			return "Solid client";
+		}
+		public function getDisplayNames($search = "", $limit = null, $offset = null) {
+			error_log("SO getDisplayNames");
+			return [];
+		}
+		public function hasUserListings() {
+			error_log("SO hasUserListings");
+			return false;
+		}
+    	}
