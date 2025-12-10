583cb041b3cbabd982e3185b3026875120b11aff
Yvo Brevoort <yvo@muze.nl>
2025-12-10 16:10:08 +0100

    remove type

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ php console.php config:system:set trusted_domains 1 --value=server
 php console.php config:system:set trusted_domains 2 --value=nextcloud.local
 php console.php config:system:set trusted_domains 3 --value=thirdparty
 # set 'tester' and 'https://tester' as tyrusted apps for the test suite to run
-php console.php config:app:set solid trustedApps --type=string --value='["https://tester", "tester"]'
+php console.php config:app:set solid trustedApps --value='["https://tester", "tester"]'
 
 echo configured
 mkdir -p /var/www/html/data/files_trashbin/versions
